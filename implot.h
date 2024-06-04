e81dcadb214447565b2ebd6358267d9260b97a8e
Musa Mahmood <Musasmahmood@gmail.com>
2024-06-04 07:16:02 -0400

    Add bindings for Jai

--- implot.h
+++ implot.h
@@ -855,6 +855,12 @@ IMPLOT_TMP void PlotLine(const char* label_id, const T* values, int count, doubl
 IMPLOT_TMP void PlotLine(const char* label_id, const T* xs, const T* ys, int count, ImPlotLineFlags flags=0, int offset=0, int stride=sizeof(T));
 IMPLOT_API void PlotLineG(const char* label_id, ImPlotGetter getter, void* data, int count, ImPlotLineFlags flags=0);
 
+// External bindings: 
+IMPLOT_API void PlotLineF(const char* label_id, const float* values, int count, float xscale=1, float x0=0, int offset=0, int stride=sizeof(float));
+IMPLOT_API void PlotLineF(const char* label_id, const float* xs, const float* ys, int count, ImPlotLineFlags flags=0, int offset=0, int stride=sizeof(float));
+IMPLOT_API void PlotLineD(const char* label_id, const double* values, int count, double xscale=1, double x0=0, int offset=0, int stride=sizeof(double));
+IMPLOT_API void PlotLineD(const char* label_id, const double* xs, const double* ys, int count, ImPlotLineFlags flags=0, int offset=0, int stride=sizeof(double));
+
 // Plots a standard 2D scatter plot. Default marker is ImPlotMarker_Circle.
 IMPLOT_TMP void PlotScatter(const char* label_id, const T* values, int count, double xscale=1, double xstart=0, ImPlotScatterFlags flags=0, int offset=0, int stride=sizeof(T));
 IMPLOT_TMP void PlotScatter(const char* label_id, const T* xs, const T* ys, int count, ImPlotScatterFlags flags=0, int offset=0, int stride=sizeof(T));
